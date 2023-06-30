7fbda33a096e1a37aa397dee243b4314e3400950
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-06-30 16:02:57 +0600

    Update Contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,16 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  await lock.depolyed();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
