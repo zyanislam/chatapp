596813658f8b530f91d3390726cde9e949e8251d
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-07-28 19:26:29 +0600

    Update ChatBox.sol

--- contracts/ChatBox.sol
+++ contracts/ChatBox.sol
@@ -1,15 +1,24 @@
 //Smart Contract
 
 // SPDX-License-Identifier: MIT
-pragma solidity >= 0.7.0 <0.9.0
+pragma solidity >=0.7.0 <0.9.0;
 
-contract ChatBox{
-    struct user{
+contract ChatBox {
+    struct user {
         string name;
         friend[] friendList;
     }
 
-    
-    
+    struct friend {
+        address pubkey;
+        string name;
+    }
+    struct message {
+        address sender;
+        uint256 timestamp;
+        string message;
+    }
 
+    mapping(address => user) userList;
+    mapping(bytes => user) userList;
 }
