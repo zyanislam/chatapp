1ec24b7d4334b8dc2b2c4c30ae9cf2acb3245ce3
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-08-07 04:20:39 +0600

    Update apiFeature.js

--- Utils/apiFeature.js
+++ Utils/apiFeature.js
@@ -12,6 +12,44 @@ export const CheckIfWalletIsConnected = async () => {
         return firstAccount;
     }
     catch (error) {
-        
+        console.log(error);
     }
- };
+};
+ 
+export const ConnectMyWallet = async () => { 
+    try {
+        if (!window.ethereum) return console.log("First Install MetaMask");
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+        });
+        const firstAccount = accounts[0];
+        return firstAccount;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getContract = (signerOrProvider) => new ethers.Contract(ChatAppAddress, ChatAppABI, signerOrProvider);
+
+// General Functions required to have a connection & communication with the SmartContract
+export const ConnectingWithContract = async () => {
+    try {
+        const web3modal = new Web3Modal();
+        const connection = await web3modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
+        const contract = fetchContract(signer);
+
+        return contract;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Curates the Readable timestamp for the messages
+export const timeConversion = (time) => {
+    const newTime = new Date(time.toNumber());
+    const realTime = newTime.getHours() + ":" + newTime.getMinutes() + ":" + newTime.getSeconds() + "Date: " + newTime.getDate() + "/" + (newTime.getMonth() + 1) + "/" + newTime.getFullYear();
+    
+    return realTime;
+}
