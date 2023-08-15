34db69f6719ea75858b1e9f34ad7e02e6ff3d65b
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-08-15 23:18:39 +0600

    Update ChatAppContext.js

--- Context/ChatAppContext.js
+++ Context/ChatAppContext.js
@@ -62,7 +62,7 @@ export const ProviderApp = ({ children }) => {
     };
 
     // Create Account
-    const createAccount = async ({ name, accountAddress }) => { 
+    const createAccount = async ({ name, accountAddress }) => {
         try {
             if (name || accountAddress) return setError("Name & Account Address cannot be empty!")
             const contract = await ConnectingWithContract();
@@ -74,7 +74,21 @@ export const ProviderApp = ({ children }) => {
         } catch (error) {
             setError("ERROR!! Please create your account first!");
         }
-    }
+    };
+
+    // Add your friends
+    const addFriends = async ({name, accountAddress}) => {
+        try {
+            if (name || accountAddress) return setError("Please provide the Name & Friend Address");
+
+            const contract = await ConnectingWithContract();
+            const addMyFriend = await contract.addFriend(accountAddress, name);
+            
+        } catch (error) {
+            setError("Something went wrong when you tried to add friends!! Try again.");
+        }
+    };
+
     return (
         <ContextApp.Provider value={{ readMessage, createAccount }}>
             {children}
