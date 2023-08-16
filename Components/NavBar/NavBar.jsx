c471aa868e25d60459ddaaf1426f41d8832eb6be
Towhid Sarwer Islam <107320903+Towhid173@users.noreply.github.com>
2023-08-16 21:13:53 +0600

    Update NavBar

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -31,7 +31,6 @@ const NavBar = () => {
         },
     ];
     //UseState
-    //const [active. setActive] = useState(2);
      const [active. setActive] = useState(2);
      const [open. setOpen] = useState(false);
      const [openModel. setOpenModel] = useState(false);
@@ -44,6 +43,7 @@ const NavBar = () => {
                 <Image src={images.logo} alt="logo" width={50} heigh={50}>
             </div>
             <div className={Style.NavBar_Box_right}>
+                {/* //DESKTOP */}
             <div className={Style.NavBar_Box_right_menu}>
                 {menuItems.map((el, i)=> (
                     <div onClick={()=> setActive(i + 1)} key={1+1} className={`$
@@ -60,8 +60,61 @@ const NavBar = () => {
                 ))}
             
                 </div>
+                {/* //MOBILE */}
+                { open && (
+                                <div className={Style.mobile_menu}>
+                                {menuItems.map((el, i)=> (
+                                    <div onClick={()=> setActive(i + 1)} key={1+1} 
+                                    className={`${Style.mobile_menu_items} ${
+                                        active == i + 1 ? Style.active_btn : ""
+                            
+                                    }}`}
+                                    >
+                                        <Link className={Style.mobile_menu_items_link}
+                                        href={el.link}
+                                        >
+                                            {el.menu}
+                                        </Link>
+                                    </div>
+                ))}
+
+                <p className={Style.mobile_menu_btn}>
+                    <Image src={images.close} alt="close" width={50} heigh={50}
+                    onClick={()=> setOpen(flase)}
+                    />
+                </p>
+            </div>
+                )}
+             {/* CONNECT WALLET */}
+             <div className={Style.NavBar_Box_right_connect}>
+             {account== "" ?(
+                <button onClick={()=> connetWallet()}>
+                {""}
+                <span>Connect Wallet</span> 
+                </button>
+             ) :(
+                <button onClick={()=> setOpenModel(true)}>
+                    {""}
+                    <Image src={userName ? images.accountName : images.create2}
+                    alt="Account Image"
+                    width={20}
+                    heigh={20} 
+                    />
+                    {""}
+                    <small>{userName || "Create Account"}</small>
+                </button>
+             )}   
+            </div> 
+            <div
+             className={Style.NavBar_Box_right_open}
+                onClick={()=> setOpen(true)}
+            >
+                <Image src={images.open} alt="open" width={30} heigh={30}
+            </div>
+
             </div>
             </div>
+             </div>
     );
 
 };
