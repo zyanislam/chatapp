70acc80d53a73a8302c5abffa0decc7eaa8df225
Towhid Sarwer Islam <107320903+Towhid173@users.noreply.github.com>
2023-08-21 22:56:22 +0600

    UserCard update

--- Components/UserCard/UserCard.jsx
+++ Components/UserCard/UserCard.jsx
@@ -10,7 +10,9 @@ const UserCard = ({el, i, addFriends }) => {
   return (
     <div className={Style.UserCard}>
       <div className={Style.UserCard_box}>
-        <Image src={images[`image${i+1}`]}
+        <Image
+        className={Style.UserCard_box_img}
+         src={images[`image${i+1}`]}
           alt="user"
           width={100}
           height={100}
