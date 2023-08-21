d6cc94b5f89c6c84c05a4be8d41114fabdcfab78
Towhid Sarwer Islam <107320903+Towhid173@users.noreply.github.com>
2023-08-21 23:44:00 +0600

    Update Filter.jsx

--- Components/Filter/Filter.jsx
+++ Components/Filter/Filter.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import Image from "next/image";
+
+//Internal Import
+import Style from './Filter.module.css';
+import images from '../../assets';
+import {ChatAppContext} from '../../Context/ChatAppContext';
+import {Model} from "../index";
 
 const Filter = () => {
+  const {account, addFriends } = useContext(ChatAppContext);
+  //USESTATE
+  const [addFriend, setFriend] = useState(false);
   return (
-    <div>Filter</div>
+    <div className={Style.Filter}>
+      <div className={Style.Filter_box}>
+        <div className={Style.Filter_box_left}>
+          <div className={Style.Filter_box_left_search}>
+            <Image src={images.search} alt="image" width={20} height={20}
+            <input type="text" placeholder="search.." />
+          </div>
+        </div>
+        <div className={Style.Filter_box_right}>
+          <button>
+            <Image src={images.clear} alt="clear" width={20} height={20}
+            CLEAR CHAT
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
