d03fba620ff00583cad1805ec8deee5e40edf609
Towhid Sarwer Islam <107320903+Towhid173@users.noreply.github.com>
2023-08-21 22:16:38 +0600

    Allusers Update

--- pages/allusers.js
+++ pages/allusers.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect, useContext} from 'react';
+
+//Internal Import
+import { UserCard } from "../Components/index";
+import Style from '../styles/allusers.module.css';
+import { ChatAppContect } from "../Context/ChatAppContext";
 
 const allusers = () => {
+
+  const {userLists addFriends } = userContext(ChatAppContext);
   return (
-    <div>allusers</div>
+    <div>
+      <dev className={Style.alluser_info}>
+        <h1>Find Your Friends </h1>
+      </dev>
+      <dev className={Style.alluser}>
+        {userLists.map((el, i)=>(
+          <UserCard key={i + 1} el={el} i={i} addFriends={addFriends}/>
+        ))}
+      </dev>
+      </div>
   )
 }
 
