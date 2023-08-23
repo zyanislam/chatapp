3bff5a957121f910d0ac812506bddb5d2c3c0435
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-08-23 23:54:48 +0600

    Update Filter

--- Components/Filter/Filter.jsx
+++ Components/Filter/Filter.jsx
@@ -4,48 +4,55 @@ import Image from "next/image";
 //Internal Import
 import Style from './Filter.module.css';
 import images from '../../assets';
-import {ChatAppContect} from '../../Context/ChatAppContext';
-import {Model} from "../index";
+import { ChatAppContect } from '../../Context/ChatAppContext';
+import { Model } from "../index";
 
 const Filter = () => {
   const {account, addFriends } = useContext(ChatAppContect);
   const { } = useContext(ChatAppContect);
-  //USESTATE
-  const [addFriend, setFriend] = useState(false);
+  const [addFriend, setAddFriend] = useState(false);
   return (
     <div className={Style.Filter}>
-      <div className={Style.Filter_box}>
-        <div className={Style.Filter_box_left}>
-          <div className={Style.Filter_box_left_search}>
-            <Image src={images.search} alt="image" width={20} height={20}
+      <div className={Style.Filter_Box}>
+        <div className={Style.Filter_Box_left}>
+          <div className={Style.Filter_Box_left_search}>
+            <Image src={images.search} alt="image" width={20} height={20} />
             <input type="text" placeholder="search.." />
           </div>
         </div>
-        <div className={Style.Filter_box_right}>
+        <div className={Style.Filter_Box_right}>
           <button>
-            <Image src={images.clear} alt="clear" width={20} height={20}
-            CLEAR CHAT
+            <Image
+              src={images.clear}
+              alt="clear"
+              width={20}
+              height={20}
+              CLEAR CHAT
+            />
           </button>
           <button onClick={()=> setAddFriend(true)}>
-            <Image src={images.user} alt="clear" width={20} height={20}
-            ADD FRIEND
+            <Image
+              src={images.user}
+              alt="clear"
+              width={20}
+              height={20}
+              ADD FRIEND
+            />
           </button>
         </div>
       </div>
-      {/* // Model Component */}
+
+      {/* Model Component */}
       {addFriend && (
         <div className={Style.Filter_model}>
           <Model
             openBox={setAddFriend}
-            title="WELCOME TO"
-              head="CHAT BUDDY"
-              info="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Ea adipisci fugiat est optio laudantium praesentium repudiandae vitae 
-              voluptatibus quae, animi odit architecto? Voluptatum blanditiis quasi ipsum. 
-              Qui adipisci magnam eaque." 
-              smallInfo="Kindly Select Your Friend Name & Address.."
-              image={images.hero}
-              functionName={addFriends}
+            title="Welcome To"
+            head="Ripples"
+            info="Lorem ipsum dolor sit amet consectetur adipisicing elit." 
+            smallInfo="Select Your Friend Name & Address.."
+            image={images.hero}
+            functionName={addFriends}
           />
         </div>
       )}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import React, {useEffect, useState, useContext} from 'react'
 import { ChatAppContect } from "../Context/ChatAppContext";
+import { Filter, Friend } from "../Components/index";
+
+
 const index = () => {
-    const { } = useContext(ChatAppContect);
+    // const { } = useContext(ChatAppContect);
     return (
-        <div></div>
+        <div>
+            <Filter />
+            <Friend />
+        </div>
     )
 };
 
