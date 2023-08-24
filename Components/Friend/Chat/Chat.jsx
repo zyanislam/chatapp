e702c2eee360acfb12c4deabfc42ec9aadef0a7f
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-08-24 09:44:54 +0600

    Update Chat.jsx

--- Components/Friend/Chat/Chat.jsx
+++ Components/Friend/Chat/Chat.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 import Style from './Chat.module.css';
+import images from '../../../assets';
+import { converTime } from "../../../Utils/apiFeature";
+import { Loader } from "../../index";
 
+const Chat = ({ functionName, readMessage, friendMsg, account, userName, loading, currentUserName, currentUserAddress }) => {
+  const [message, setMessage] = useState('');
+  const [chatData, setChatData] = useState({
+    name: "",
+    address: "",
+  });
+  const router = useRouter();
+  useEffect(() => {
+    if (!router.isReady) return;
+    setChatData(router.query);
+  }, [router.isReady]);
 
-const Chat = () => {
   return (
-    <div>Chat</div>
+    <div className={Style.Chat}>
+      {currentUserName && currentUserAddress ? (
+        <div>
+          
+        </div>
+      )}
+    </div>
   )
-}
+};
 
-export default Chat
+export default Chat;
