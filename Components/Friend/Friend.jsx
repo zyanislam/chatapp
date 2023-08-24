bf993f4ff4f4894a75450a11f8343e28a572f197
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-08-24 08:49:51 +0600

    Update Friend.jsx

--- Components/Friend/Friend.jsx
+++ Components/Friend/Friend.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import Image from "next/image";
+import Style from './Friend.module.css';
+import Card from './Card/Card';
+import Chat from './Chat/Chat';
+import { ChatAppContect } from '../../Context/ChatAppContext';
+
+
 
 const Friend = () => {
   return (
