ef9eab4eef9cb3d21f7ecd0c6c0dfb58585206dd
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-08-24 08:59:44 +0600

    Update Friend.jsx

--- Components/Friend/Friend.jsx
+++ Components/Friend/Friend.jsx
@@ -9,9 +9,35 @@ import { ChatAppContect } from '../../Context/ChatAppContext';
 
 const Friend = () => {
   const array = [1, 2, 3, 4, 5, 6];
-  const { sendMessage, account, friendLists, readMessage, readUser, userName, loading, currentUserName, currentUserAddress } = useContext(ChatAppContect)
+  const { sendMessage, account, friendLists, friendMsg, readMessage, readUser, userName, loading, currentUserName, currentUserAddress } = useContext(ChatAppContect)
   return (
-    <div>Friend</div>
+    <div className={Style.Friend}>
+      <div className={Style.Friend_Box}>
+        <div className={Style.Friend_Box_left}>
+          {friendLists.map((el, i) => (
+            <Card
+              key={i + 1}
+              el={el}
+              i={i}
+              readMessage={readMessage}
+              readUser={readUser}
+            />
+          ))}
+        </div>
+        <div className={Style.Friend_Box_right}>
+          <Chat
+            functionName={sendMessage}
+            readMessage={readMessage}
+            friendMsg={friendMsg}
+            account={account}
+            userName={userName}
+            loading={loading}
+            currentUserName={currentUserName}
+            currentUserAddress={currentUserAddress}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
